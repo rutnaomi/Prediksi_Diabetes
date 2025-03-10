1aab39891ef878dbe88f47a4e832666ee18b8e72
Rut Sitompul <rutnaomiestersitompul@gmail.com>
2025-03-11 01:37:05 +0800

    second commit (modified)

--- prediksiJantung.php
+++ prediksiJantung.php
@@ -3,31 +3,34 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $input_tekanan = intval($_POST["tekanan_darah"]);
 
-    // Data pasangan (tekanan darah sistolik, kondisi jantung)
-    $data = [
-        [105, 1], [95, 1], [115, 1], [102, 1], [125, 1], 
-        [135, 0], [144, 0], [171, -1], [200, -1], [230, -1], 
-        [180, -1], [175, -1], [112, 1], [145, 0], [161, 0], 
-        [92, 1], [99, 1], [133, 0], [139, 0], [128, 1]
-    ];
+    if ($input_tekanan < 90) {
+        echo "<h2>Hasil Prediksi</h2>";
+        echo "<p>Tekanan Darah: <strong>$input_tekanan mmHg</strong></p>";
+        echo "<p>Status Kesehatan Jantung Andaa: <strong>Di luar prediksi. Tekanan darah terlalu rendah!😮 Yang bener ajee?!</strong></p>";
+    } else {
+        // Data pasangan (tekanan darah sistolik, kondisi jantung)
+        $data = [
+            [105, 1], [95, 1], [115, 1], [102, 1], [125, 1], 
+            [135, 0], [144, 0], [171, -1], [200, -1], [230, -1], 
+            [180, -1], [175, -1], [112, 1], [145, 0], [161, 0], 
+            [92, 1], [99, 1], [133, 0], [139, 0], [128, 1]
+        ];
 
-    // Inisialisasi variabel
-    $prediksi = "Tidak Diketahui";
-    $min_selisih = PHP_INT_MAX;
+        $min_selisih = PHP_INT_MAX;
 
-    // Mencari nilai tekanan darah terdekat
-    foreach ($data as $pasien) {
-        $tekanan = $pasien[0];
-        $kondisi = $pasien[1];
+        // nah ini untuk mencari nilai tekanan darah terdekat
+        foreach ($data as $pasien) {
+            $tekanan = $pasien[0];
+            $kondisi = $pasien[1];
 
-        $selisih = abs($tekanan - $input_tekanan);
-        if ($selisih < $min_selisih) {
-            $min_selisih = $selisih;
-            $status = $kondisi;
+            $selisih = abs($tekanan - $input_tekanan);
+            if ($selisih < $min_selisih) {
+                $min_selisih = $selisih;
+                $status = $kondisi;
+            }
         }
-    }
 
-        // Menentukan status kesehatan
+        // Menentukan status kesehatan pasiennya
         if ($status == 1) {
             $prediksi = "Sehat. Terus jaga kesehatanmuu, jangan lupa makan buah dan sayur bro🍎💪";
         } elseif ($status == 0) {
@@ -40,5 +43,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<p>Tekanan Darah: <strong>$input_tekanan mmHg</strong></p>";
         echo "<p>Status Kesehatan Jantung Andaa: <strong>$prediksi</strong></p>";
     }
+}
 
 ?>
